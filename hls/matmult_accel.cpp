9413c82657ff538c40bed316556468a83c77ed36
Diego Sandoval <dsandoval@semtech.com>
2020-04-10 22:25:35 +0200

    Code formatting

--- hls/matmult_accel.cpp
+++ hls/matmult_accel.cpp
@@ -2,129 +2,118 @@
 Copyright (c) 2016, Xilinx, Inc.
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, 
+1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
 
-3. Neither the name of the copyright holder nor the names of its contributors 
-may be used to endorse or promote products derived from this software 
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
 without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ***************************************************************************/
 #include "matmult.h"
 
-
 // --------------------------------------------------------
 // function to be accelerated in HW
-template <typename T>
-void mmult_hw(T a[N][N], T b[N][N], T out[N][N])
-{
-	// matrix multiplication of a A*B matrix
-	L1:for (int ia = 0; ia < N; ++ia)
-		L2:for (int ib = 0; ib < N; ++ib)
-		{
-			T sum = 0;
-			L3:for (int id = 0; id < N; ++id)
-				sum += a[ia][id] * b[id][ib];
-			out[ia][ib] = sum;
-		}
+template <typename T> void mmult_hw(T a[N][N], T b[N][N], T out[N][N]) {
+L1:
+  for (int ia = 0; ia < N; ++ia)
+  L2:
+    for (int ib = 0; ib < N; ++ib) {
+      T sum = 0;
+    L3:
+      for (int id = 0; id < N; ++id)
+        sum += a[ia][id] * b[id][ib];
+      out[ia][ib] = sum;
+    }
 
-		return;
+  return;
 }
 
-
-template<typename T>
-void axis2Mat (axis_t *src, 
-			     T A[N][N],
-				 T B[N][N])
-				 {
+template <typename T> void axis2Mat(axis_t *src, T A[N][N], T B[N][N]) {
 #pragma HLS inline off
-	union
-	{
-		int ival;
-		T oval;
-	} converter;
+  union {
+    int ival;
+    T oval;
+  } converter;
 
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
 #pragma HLS pipeline
 #pragma HLS loop_flatten off
-			int k = i*N + j;
-			converter.ival = src[k].data;
-			A[i][j] = converter.oval;
-		}	
-	}
+      int k = i * N + j;
+      converter.ival = src[k].data;
+      A[i][j] = converter.oval;
+    }
+  }
 
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
 #pragma HLS pipeline
 #pragma HLS loop_flatten off
-			int k = i*N + j;
-			converter.ival = src[k + SIZE].data;
-			B[i][j] = converter.oval;
-		}	
-	}
+      int k = i * N + j;
+      converter.ival = src[k + SIZE].data;
+      B[i][j] = converter.oval;
+    }
+  }
 }
 
-template<typename T>
-void Mat2axis (T C[N][N],
-		 axis_t *dst
-		 ) {
+template <typename T> void Mat2axis(T C[N][N], axis_t *dst) {
 #pragma HLS inline off
-	union
-	{
-		int oval;
-		T ival;
-	} converter;
+  union {
+    int oval;
+    T ival;
+  } converter;
 
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
 #pragma HLS pipeline
 #pragma HLS loop_flatten off
-			ap_uint<1> tmp = 0;
-			if ((i == N-1) && (j == N-1)) {
-				tmp = 1;
-			}
-			dst[i*N+j].last = tmp;
-			converter.ival = C[i][j];
-			dst[i*N+j].data = converter.oval;
-		}
-	}
+      ap_uint<1> tmp = 0;
+      if ((i == N - 1) && (j == N - 1)) {
+        tmp = 1;
+      }
+      dst[i * N + j].last = tmp;
+      converter.ival = C[i][j];
+      dst[i * N + j].data = converter.oval;
+    }
+  }
 }
 
 extern "C" {
-void matmult_accel (axis_t *src, axis_t *dst) {
-	
-#pragma HLS INTERFACE axis port=src
-#pragma HLS INTERFACE axis port=dst
-#pragma HLS INTERFACE s_axilite port=return
+void matmult_accel(axis_t *src, axis_t *dst) {
+
+#pragma HLS INTERFACE axis port = src
+#pragma HLS INTERFACE axis port = dst
+#pragma HLS INTERFACE s_axilite port = return
 
 #pragma HLS dataflow
 
-	float A[N][N];
-	float B[N][N];
-	float C[N][N];
+  float A[N][N];
+  float B[N][N];
+  float C[N][N];
 
-	axis2Mat(src, A, B);	
+  axis2Mat(src, A, B);
 
-	mmult_hw(A, B, C);
+  mmult_hw(A, B, C);
 
-	Mat2axis(C, dst);	
+  Mat2axis(C, dst);
 }
 }
--- hls/matmult_tb.cpp
+++ hls/matmult_tb.cpp
@@ -1,68 +1,63 @@
+#include "matmult.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "matmult.h"
-
-typedef float T ;
 
 
-void mmult_sw(T a[N][N], T b[N][N], T out[N][N])
-{
-	// matrix multiplication of a A*B matrix
-	for (int ia = 0; ia < N; ++ia)
-		for (int ib = 0; ib < N; ++ib)
-		{
+typedef float T;
 
-			float sum = 0;
+void mmult_sw(T a[N][N], T b[N][N], T out[N][N]) {
+  // matrix multiplication of a A*B matrix
+  for (int ia = 0; ia < N; ++ia)
+    for (int ib = 0; ib < N; ++ib) {
 
-			for (int id = 0; id < N; ++id)
+      float sum = 0;
 
-				sum += a[ia][id] * b[id][ib];
+      for (int id = 0; id < N; ++id)
 
-			out[ia][ib] = sum;
-		}
+        sum += a[ia][id] * b[id][ib];
+
+      out[ia][ib] = sum;
+    }
 }
 
+int main(void) {
 
-int main(void)
-{
+  int ret_val = 0;
 
-	int ret_val = 0;
+  int i, j, err;
 
-	int i,j, err;
+  T matOp1[N][N];
+  T matOp2[N][N];
+  T matMult_sw[N][N];
+  T matMult_hw[N][N];
 
-	T matOp1[N][N];
-	T matOp2[N][N];
-	T matMult_sw[N][N];
-	T matMult_hw[N][N];
+  /** Matrix Initiation */
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      matOp1[i][j] = (float)(i + j);
 
-	/** Matrix Initiation */
-	for(i = 0; i<N; i++)
-		for(j = 0; j<N; j++)
-			matOp1[i][j] = (float)(i+j);
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      matOp2[i][j] = (float)(i * j);
+  /** End of Initiation */
 
-	for(i = 0; i<N; i++)
-		for(j = 0; j<N; j++)
-			matOp2[i][j] = (float)(i*j);
-	/** End of Initiation */
+  printf("NORMAL MODE\r\n");
+  mmult_hw(matOp1, matOp2, matMult_hw);
 
-	printf("NORMAL MODE\r\n");
-	mmult_hw(matOp1, matOp2, matMult_hw);
+  /* reference Matrix Multiplication */
+  mmult_sw(matOp1, matOp2, matMult_sw);
 
-	/* reference Matrix Multiplication */
-	mmult_sw(matOp1, matOp2, matMult_sw);
+  /** Matrix comparison */
+  err = 0;
+  for (i = 0; (i < N && !err); i++)
+    for (j = 0; (j < N && !err); j++)
+      if (matMult_sw[i][j] != matMult_hw[i][j])
+        err++;
 
-	/** Matrix comparison */
-	err = 0;
-	for (i = 0; (i<N && !err); i++)
-		for (j = 0; (j<N && !err); j++)
-			if (matMult_sw[i][j] != matMult_hw[i][j])
-				err++;
+  if (err == 0)
+    printf("Matrixes identical ... Test successful!\r\n");
+  else
+    printf("Test failed!\r\n");
 
-	if (err == 0)
-		printf("Matrixes identical ... Test successful!\r\n");
-	else
-		printf("Test failed!\r\n");
-
-	return err;
-
+  return err;
 }
